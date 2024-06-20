79b8be684b1fc527e6ff989447b19975d847d227
Henry Tran <161263637+henrytran07@users.noreply.github.com>
2024-06-20 15:20:41 -0700

    Add files via upload

---
+++ verification_information.h
@@ -0,0 +1,7 @@
+
+#ifndef VERIFICATION_INFORMATION_H
+#define VERIFICATION_INFORMATION_H
+
+bool verification(string& valid_name, string& valid_ssn, int& valid_fico_score);
+bool ssnValidInput(string input);
+#endif
